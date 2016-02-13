749c3897c5cd7ec6ab3ad9a8149913189195b37a
Michael Gilbertson <mgg9xv@virginia.edu>
2016-02-13 14:35:25 -0500

    Almost finishes product

--- src/js/main.js
+++ src/js/main.js
@@ -4,8 +4,9 @@
     $(document).ready(function(){
         console.log('jQuery is loaded and ready to go...');
         $(document).on('click', '.arrow', function(){
+            $(this).toggleClass('invert');
             $(this).parent().parent().toggleClass('full-height');
-            $(this).parent().parent().find('div.featured-work-description').toggleClass('hidden-away');
+            $(this).parent().parent().find('div.featured-work-description-container').toggleClass('hidden-away');
         })
     });
 })();
