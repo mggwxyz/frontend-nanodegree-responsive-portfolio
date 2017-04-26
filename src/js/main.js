5c6cf8909d8f4c07df01939ddfb343e4d45be3b6
Michael Gilbertson <mgg9xv@virginia.edu>
2017-04-26 16:56:49 -0400

    Removed jquery

--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,44 @@
 
     "use strict";
 
-    $(document).ready(function(){
-        $(document).on('click', '.actions-icon', function(){
-            $(this).toggleClass('invert');
-            $(this).parent().parent().toggleClass('full-height');
-            $(this).parent().parent().find('div.featured-work-description-container').toggleClass('hidden-away');
-        })
-    });
+    function addEventListeners() {
+
+        console.log('adding event listeners');
+
+        // Toggle class
+        function toggleClass(element, classToBeToggled){
+            var elementClasses = element.className.toString();
+            var classRegex = new RegExp('\\b' + classToBeToggled + '\\b');
+            var elementHasClass = elementClasses.match(classRegex);
+            if (elementHasClass) {
+                element.className = elementClasses.replace(classRegex, '');
+            } else {
+                element.className = elementClasses + " " + classToBeToggled;
+            }
+        }
+
+        // Find all the project info buttons
+        var actionIcons = document.querySelectorAll('.actions-icon');
+
+        for ( var i = 0; i < actionIcons.length; i++){
+            actionIcons[i].addEventListener('click', function(event) {
+                var el = event.currentTarget;
+                var grandParent = el.parentNode.parentNode;
+                var descriptionContainer = grandParent.querySelector('div.featured-work-description-container');
+                toggleClass(grandParent, 'full-height');
+                toggleClass(descriptionContainer, 'hidden-away');
+            });
+        }
+    }
+
+    /*
+        'complete means the document and all its subresources have finished loading and the 'load' event is about to be fired
+        'loading' means the document is still loading
+     */
+    if (document.readyState === 'complete' || (document.readyState === 'loading' && !document.documentElement.doScroll)) {
+        addEventListeners();
+    } else {
+        document.addEventListener('DOMContentLoaded', addEventListeners);
+    }
 
 })();
